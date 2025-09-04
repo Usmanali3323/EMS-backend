c44c4a6f52dcb972a998dc2bfc67ebadce80b790
Usman Ali <108189887+Usmanali3323@users.noreply.github.com>
2025-09-04 22:23:56 +0500

    Update generateToken.js

--- util/generateToken.js
+++ util/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = async(empId)=>{
         return false
     }
         const token = jwt.sign({_id:user._id, role:user.role, email:user.email}, process.env.JWT_SECRET, { 
-          expiresIn: '3h' 
+          expiresIn: '5h' 
         });
 return {acessToke:token}
     }
